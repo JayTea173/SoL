6eb61b60495ea8ceac8cbac6a11b89b26cf6259f
JayTea <jaytea173@googlemail.com>
2020-06-05 20:43:27 +0200

    Rev7

---
+++ Secret Of Lama/Assets/Scripts/SoL/Actors/DialogPartner.cs
@@ -0,0 +1,21 @@
+﻿using SoL.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SoL.Actors
+{
+    public class DialogPartner : MonoBehaviour
+    {
+        public Dialog dialog;
+        public AudioClip voice;
+
+        public void StartDialog(DialogPartner partner)
+        {
+            DialogUI.Instance.Display(dialog, partner, this);
+        }
+    }
+}
--- Secret Of Lama/Assets/Scripts/SoL/Engine.cs
+++ Secret Of Lama/Assets/Scripts/SoL/Engine.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace SoL
 {
@@ -11,6 +12,7 @@ namespace SoL
 
         private const string gameObjectName = "Engine";
         private static Engine instance;
+        private static System.Random r;
         public static Engine Instance
         {
             get
@@ -46,7 +48,10 @@ namespace SoL
             DontDestroyOnLoad(go);
 
             
+
+            
             instance = go.GetComponent<Engine>();
+            r = new System.Random();
             if (instance == null)
                 instance = go.AddComponent<Engine>();
 
@@ -61,5 +66,45 @@ namespace SoL
 
             return new Vector3(pixelPosition.x / Engine.pixelsPerUnit, pixelPosition.y / Engine.pixelsPerUnit, v.z);
         }
+
+        #region Random
+        public static double Gaussian(double center = 0d, double pioffset = 1d)
+        {
+            return NextGaussian() * pioffset + center;
+        }
+
+        public static float Gaussian(float center = 0f, float pioffset = 1f)
+        {
+            return (float)NextGaussian() * pioffset + center;
+        }
+        //this will go -pi to +pi with center at 0
+        private static double NextGaussian(double mu = 0, double sigma = 1)
+        {
+            var u1 = r.NextDouble();
+            var u2 = r.NextDouble();
+
+            var rand_std_normal = System.Math.Sqrt(-2.0 * System.Math.Log(u1)) *
+                                System.Math.Sin(2.0 * System.Math.PI * u2);
+
+            var rand_normal = mu + sigma * rand_std_normal;
+
+            return rand_normal;
+        }
+
+        public static float RandomFloat(float min = 0f, float max = 1f)
+        {
+            return (float)(r.NextDouble() * (max - min)) + min;
+        }
+
+        public static int RandomInt(int min = int.MinValue, int max = int.MaxValue)
+        {
+            return r.Next(min, max);
+        }
+
+        public static double RandomDouble()
+        {
+            return r.NextDouble();
+        }
+        #endregion
     }
 }
